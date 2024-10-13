78df31c135ad0e6456dc505e3f3cebb5194db972
NuttyJamie <jongmie91@gmail.com>
2024-10-13 23:24:45 +0900

    Moved files to proper directories

--- calculator/utils.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def round_result(value, precision):
-	format_string = "{:." + str(precision) + "f}"
-	return format_string.format(value)
-
-def convert_to_radians(angle: float, unit: str = 'degree'):
-	math.radians(angle)
